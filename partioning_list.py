897848c66f27beedb733f36d0ce85882a3d8bcb2
Aditya Sharma <adityasma@gmail.com>
2018-11-25 09:11:53 +0530

    Update partioning_list.py

--- partioning_list.py
+++ partioning_list.py
@@ -4,3 +4,4 @@ for i in range(1,len(l)):
     #checking whether sum of sub lists are equal.
     if sum(l[:i])==sum(l[i:]):
         print("list can be partitioning with: ",(l[:i],l[i:]))
+        break
